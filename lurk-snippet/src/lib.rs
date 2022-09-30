bfbdf04cd4c0d3f485ed43127602de37e532af0d
samuelburnham <samuel.burnham@gmail.com>
2022-09-30 12:45:12 -0400

    Add local unit tests and web test template

--- lurk-snippet/src/lib.rs
+++ lurk-snippet/src/lib.rs
@@ -1,103 +1,27 @@
+pub mod repl;
 mod utils;
-/// Module for wasm-bindgen specific handling and endpoints.
-use lurk::{
-    eval::{empty_sym_env, Evaluator},
-    proof::nova,
-    store::{ContTag, Pointer, Store},
-    writer::Write,
-};
-use serde_json::json;
-use std::collections::HashMap;
-use wasm_bindgen::prelude::*;
-// use blstrs::Scalar as Fr;
-//use pasta_curves::pallas::Scalar as P;
-pub use wasm_bindgen_rayon::init_thread_pool;
 
-// When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
-// allocator.
-#[cfg(feature = "wee_alloc")]
-#[global_allocator]
-static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+/// Module for wasm-bindgen specific handling and endpoints.
+use wasm_bindgen::prelude::*;
+
+#[cfg(target_arch = "wasm32")]
+pub use wasm_bindgen_rayon::init_thread_pool;
 
 #[wasm_bindgen]
 pub fn init_panic_hook() {
-    utils::set_panic_hook();
+  utils::set_panic_hook();
 }
 
 #[wasm_bindgen(module = "/js/panic-handler.js")]
 extern "C" {
-    fn onPanic(message: String);
+  fn onPanic(message: String);
 }
 
 #[wasm_bindgen(start)]
 pub fn main_js() -> Result<(), JsValue> {
-    // This provides better error messages in debug mode.
-    // It's disabled in release mode so it doesn't bloat up the file size.
-    #[cfg(debug_assertions)]
-    console_error_panic_hook::set_once();
-    Ok(())
-}
-
-#[wasm_bindgen]
-pub struct Repl {
-    store: Store<nova::S1>,
-    limit: usize,
-}
-
-#[wasm_bindgen]
-impl Repl {
-    #[wasm_bindgen(constructor)]
-    pub fn new() -> Repl {
-        Repl {
-            store: Store::<nova::S1>::default(),
-            limit: 100_000_000,
-        }
-    }
-
-    /// Run a lurk snippet
-    #[wasm_bindgen]
-    pub fn execute_lurk(&mut self, source: JsValue) -> Result<JsValue, JsValue> {
-        let expression = source
-            .as_string()
-            .ok_or_else(|| "input source must be a string")?;
-
-        //let mut store = Store::<Fr>::default();
-        let mut context: HashMap<&str, String> = HashMap::new();
-
-        context.insert("expression", expression.clone());
-        if let Some(expr) = self.store.read(&expression) {
-            match Evaluator::new(
-                expr,
-                empty_sym_env(&self.store),
-                &mut self.store,
-                self.limit,
-            )
-            .eval()
-            {
-                Ok((output, iterations, _)) => {
-                    let iterations_str = iterations.to_string();
-                    context.insert("iterations", iterations_str);
-                    let result_str = match output.cont.tag() {
-                        ContTag::Outermost | ContTag::Terminal => {
-                            let result = self.store.fetch(&output.expr).clone().unwrap();
-                            result.fmt_to_string(&self.store)
-                        }
-                        ContTag::Error => "ERROR!".to_string(),
-                        _ => format!("Computation incomplete after limit: {}", self.limit),
-                    };
-
-                    context.insert("result", result_str);
-                }
-                Err(e) => {
-                    let error = format!("Evaluation Error: {}", &e);
-                    context.insert("result", error);
-                }
-            };
-        } else {
-            let error = format!("Syntax Error: {}", &expression);
-            context.insert("result", error);
-        }
-        let json = json!(&context);
-        Ok(json.to_string().into())
-    }
+  // This provides better error messages in debug mode.
+  // It's disabled in release mode so it doesn't bloat up the file size.
+  #[cfg(feature = "debug")]
+  console_error_panic_hook::set_once();
+  Ok(())
 }
---
+++ lurk-snippet/tests/app.rs
@@ -0,0 +1,28 @@
+use futures::prelude::*;
+use wasm_bindgen::JsValue;
+use wasm_bindgen_futures::JsFuture;
+use wasm_bindgen_test::{wasm_bindgen_test, wasm_bindgen_test_configure};
+use web_sys::console;
+
+extern crate lurk_snippet;
+use lurk_snippet::repl::Repl;
+
+wasm_bindgen_test_configure!(run_in_browser);
+
+// This runs a unit test in native Rust, so it can only use Rust APIs.
+// I.e. won't work with any wasm_bindgen functions
+// See src/repl.rs for equivalent unit tests
+#[test]
+fn rust_test() {
+  assert_eq!(1, 1);
+}
+
+// This runs a unit test in the browser, so it can use browser APIs.
+// Currently broken due to lack of Rust-initialized thread pool
+//#[wasm_bindgen_test]
+//fn web_test() {
+//  let mut repl = Repl::new();
+//  let lurk_expr: JsValue = "(open 123)".into();
+//  let output = repl.execute_lurk(lurk_expr).unwrap();
+//  console::log_1(&output);
+//}
