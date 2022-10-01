cc2e114705c7d4fd328a861667b5e27a92083a03
Samuel Burnham <samuel.burnham@gmail.com>
2022-10-01 00:06:34 -0400

    Formatting

--- ipfs/src/error.rs
+++ ipfs/src/error.rs
@@ -5,16 +5,16 @@ pub enum Error {
   IpfsError,
   JsonError(serde_json::Error),
   IoError(io::Error),
-} 
+}
 
 impl From<io::Error> for Error {
-    fn from(err: io::Error) -> Error {
-        Error::IoError(err)
-    }
+  fn from(err: io::Error) -> Error {
+    Error::IoError(err)
+  }
 }
 
 impl From<serde_json::Error> for Error {
-    fn from(err: serde_json::Error) -> Error {
-        Error::JsonError(err)
-    }
+  fn from(err: serde_json::Error) -> Error {
+    Error::JsonError(err)
+  }
 }
--- ipfs/src/main.rs
+++ ipfs/src/main.rs
@@ -1,38 +1,37 @@
-mod ipfs;
-mod error;
+//mod error;
+//mod ipfs;
+use ipfs::error::Error;
+use ipfs::{dag_get, dag_put};
 
-use std::path::{Path, PathBuf};
-use std::io::Write;
 use std::env;
 use std::fs::{read_to_string, File};
+use std::io::Write;
+use std::path::{Path, PathBuf};
 
-use libipld::serde::{from_ipld, to_ipld};
-use serde::Serialize;
 use blstrs::Scalar;
+use libipld::serde::{from_ipld, to_ipld};
 use lurk::field::LurkField;
 use lurk::scalar_store::ScalarStore;
 use lurk::store::{Ptr, Store};
+use serde::Serialize;
 
 use clap::{AppSettings, Args, Parser, Subcommand};
-
-use crate::ipfs::{dag_get, dag_put};
-use crate::error::Error;
 
 #[derive(Parser, Debug)]
 #[clap(version, about, long_about = None)]
 #[clap(global_setting(AppSettings::DeriveDisplayOrder))]
 struct Cli {
-    #[clap(subcommand)]
-    command: Command,
+  #[clap(subcommand)]
+  command: Command,
 }
 
 #[derive(Subcommand, Debug)]
 enum Command {
-    /// Stores Lurk data on IPFS
-    Put(Put),
+  /// Stores Lurk data on IPFS
+  Put(Put),
 
-    /// Retrieves Lurk data from IPFS
-    Get(Get),
+  /// Retrieves Lurk data from IPFS
+  Get(Get),
 }
 
 #[derive(Args, Debug)]
@@ -42,7 +41,7 @@ struct Put {
   data: PathBuf,
 
   ///IPFS host
-  #[clap(short, long, default_value = "localhost:5001")]
+  #[clap(long, default_value = "localhost:5001")]
   host: String,
 }
 
@@ -53,48 +52,48 @@ struct Get {
   cid: String,
 
   ///IPFS host
-  #[clap(short, long, default_value = "localhost:5001")]
+  #[clap(long, default_value = "localhost:5001")]
   host: String,
 }
 
 impl Put {
-    async fn put(&self) -> Result<(), Error> {
-        let store = &mut Store::<Scalar>::default();
+  async fn put(&self) -> Result<(), Error> {
+    let store = &mut Store::<Scalar>::default();
 
-        let src = read_from_path(store, &self.data)?;
-        store.hydrate_scalar_cache();
-        let (scalar_store, _) = ScalarStore::new_with_expr(store, &src);
-        let ipld = to_ipld(scalar_store.clone()).unwrap();
-        let cid = dag_put(&self.host, ipld)
-            .await
-            .expect("Failed to store on IPFS");
-        println!("{:?}\nstored on IPFS", cid);
-        Ok(())
-    }
+    let src = read_from_path(store, &self.data)?;
+    store.hydrate_scalar_cache();
+    let (scalar_store, _) = ScalarStore::new_with_expr(store, &src);
+    let ipld = to_ipld(scalar_store.clone()).unwrap();
+    let cid = dag_put(&self.host, ipld)
+      .await
+      .expect("Failed to store on IPFS");
+    println!("{:?}\nstored on IPFS", cid);
+    Ok(())
+  }
 }
 
 impl Get {
-    async fn get(&self) -> Result<(), Error> {
-        let ipld = dag_get(&self.host, &self.cid)
-            .await
-            .expect("Failed to retrieve from IPFS");
-        let data: ScalarStore<Scalar> = from_ipld(ipld).expect("Invalid Lurk IPLD");
-        let mut file = File::create("ipfs_output.txt")?;
-        file.write_all(format!("{:?}", data).as_bytes())?;
-        println!("Lurk data retrieved from IPFS");
-        Ok(())
-    }
+  async fn get(&self) -> Result<(), Error> {
+    let ipld = dag_get(&self.host, &self.cid)
+      .await
+      .expect("Failed to retrieve from IPFS");
+    let data: ScalarStore<Scalar> = from_ipld(ipld).expect("Invalid Lurk IPLD");
+    let mut file = File::create("ipfs_output.txt")?;
+    file.write_all(format!("{:?}", data).as_bytes())?;
+    println!("Lurk data retrieved from IPFS");
+    Ok(())
+  }
 }
 
 fn read_from_path<P: AsRef<Path>, F: LurkField + Serialize>(
-    store: &mut Store<F>,
-    path: P,
+  store: &mut Store<F>,
+  path: P,
 ) -> Result<Ptr<F>, Error> {
-    let path = env::current_dir()?.join(path);
-    let input = read_to_string(path)?;
-    let src = store.read(&input).unwrap();
+  let path = env::current_dir()?.join(path);
+  let input = read_to_string(path)?;
+  let src = store.read(&input).unwrap();
 
-    Ok(src)
+  Ok(src)
 }
 
 #[tokio::main]
